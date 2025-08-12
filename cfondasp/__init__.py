7c47dc9452df8c135dc18a4906a9cb71dad76f8a
ssardina <ssardina@gmail.com>
2025-08-13 09:49:35 +1000

    allow version when package not installed

--- cfondasp/__init__.py
+++ cfondasp/__init__.py
@@ -10,5 +10,11 @@
 from importlib.metadata import version
 from .utils.system_utils import get_pkg_root
 
+try:
+    # this requires the package to be installed!
+    from importlib.metadata import version, PackageNotFoundError
+    VERSION = version("cfond-asp")
+except PackageNotFoundError:
+    VERSION = "dev"  # fallback version - running with python -m lzvcup
+
 ROOT_PATH = get_pkg_root()
-VERSION = version("cfond-asp")
